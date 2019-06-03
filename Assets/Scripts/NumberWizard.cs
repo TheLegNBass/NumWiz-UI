b776fa93af3b17fd5e38f7dd16c823fde1b37bc0
Zach Armentrout <zma.armentrout@gmail.com>
2019-06-02 23:54:30 -0400

    init
    Initial Commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+    private int guess;
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] Text guessText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextGuess();
+    }
+
+    public void OnHigherPress()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnLowerPress()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+    public void NextGuess()
+    {
+        guess = UnityEngine.Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
